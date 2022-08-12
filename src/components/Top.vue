47ea912c5fdedee475054fce19d15d9244c07648
Mmy26 <yu.b1994masamura@gmail.com>
2022-08-13 00:36:52 +0900

    TailWindCSSとRouterの設定

---
+++ src/components/Top.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="nav">
+    Top
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "App",
+  components: {},
+};
+</script>
---
+++ src/router/router.ts
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import * as VueRouter from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+
+import Top from "../components/Top.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Top, // HelloWorldコンポーネントルーティング
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
